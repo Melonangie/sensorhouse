a6a7e6f2785992c55b831bbd5f175aba254e803a
angie <arabelera@hotmail.com>
2013-04-02 18:11:39 -0700

    funciones para queries de usuarios

--- public_html/funciones/usuarios.func.php
+++ public_html/funciones/usuarios.func.php
@@ -6,14 +6,9 @@ require_once ('./clases/PasswordHash.class.php');
 /**
  * Funciones para variables para encriptar claves 
  */
-
 $hash = '';
 $hasher = '';
-$hash_cost_log2 = 8;
-$pwqcheck_args = '';
 $debug = TRUE;
-$use_pwqcheck = TRUE;
-$hash_portable = FALSE;
 $dummy_salt = '$2a$08$1234567890123456789012';
 
 /**
@@ -30,36 +25,34 @@ function getPost($var) {
 
 /**
  * Oscura funcion para verificar claves y usuarios
- * Returns 'OK' if the new password/passphrase passes the requirements.
  * @param type $newpass Current passwords/passphrases
  * @param type $oldpass Old passwords/passphrases
  * @param type $user Username.
  * @param type $aux User's full name, e-mail address...
  * @param type $args Additional arguments to override the default password policy.
- * @return string
+ * @return string Returns 'OK' if the new password/passphrase passes the requirements.
  */
 function pwqcheck($newpass, $oldpass = '', $user = '', $aux = '', $args = '') {
     $retval = 'Bad passphrase (check failed)';
-    $descriptorspec = array(
+    $descripspec = array(
         0 => array('pipe', 'r'),
         1 => array('pipe', 'w'));
-
+    // Leave stderr (fd 2) pointing to where it is, likely to error_log
+    // Replace characters that would violate the protocol
     $newpass = strtr($newpass, "\n", '.');
     $oldpass = strtr($oldpass, "\n", '.');
     $user = strtr($user, "\n:", '..');
-
+    // Trigger a "too short" rather than "is the same" message in this special case
     if (!$newpass && !$oldpass)
         $oldpass = '.';
     if ($args)
         $args = ' ' . $args;
     if (!$user)
         $args = ' -2' . $args; // passwdqc 1.2.0+
-
     $command = 'exec '; // No need to keep the shell process around on Unix
     $command .= 'pwqcheck' . $args;
-    if (!($process = @proc_open($command, $descriptorspec, $pipes)))
+    if (!($process = @proc_open($command, $descripspec, $pipes)))
         return $retval;
-
     $err = 0;
     fwrite($pipes[0], "$newpass\n$oldpass\n") || $err = 1;
     if ($user)
@@ -75,50 +68,51 @@ function pwqcheck($newpass, $oldpass = '', $user = '', $aux = '', $args = '') {
         $err = 1;
     if ($err === 0 && ($status === 0 || $output !== 'OK'))
         $retval = $output;
+
     return $retval;
 }
 
 /**
- * 
- * @global type $use_pwqcheck
- * @global type $pwqcheck_args
+ * Verifica cosas de las claves
  * @param type $newpass
  * @param type $oldpass
  * @param type $user
- * @return string
+ * @return type
  */
-function my_pwqcheck($newpass, $oldpass = '', $user = '') {
-    global $use_pwqcheck, $pwqcheck_args;
-    if ($use_pwqcheck)
-        return pwqcheck($newpass, $oldpass, $user, '', $pwqcheck_args);
-
-    /* Some really trivial and obviously-insufficient password strength checks -
-     * we ought to use the pwqcheck(1) program instead. */
-    $check = '';
-    if (strlen($newpass) < 7)
-        $check = 'way too short';
-    else if (stristr($oldpass, $newpass) ||
-            (strlen($oldpass) >= 4 && stristr($newpass, $oldpass)))
-        $check = 'is based on the old one';
-    else if (stristr($user, $newpass) ||
-            (strlen($user) >= 4 && stristr($newpass, $user)))
-        $check = 'is based on the username';
-    if ($check)
-        return "Bad password ($check)";
-    return 'OK';
+function verificaClave($newpass, $oldpass = '', $user = '') {
+    return pwqcheck($newpass, $oldpass, $user, '', '');
 }
 
+/**
+ * Trae el usuario de Post y lo sanitaniza
+ * @return string Vacio si el usuario es invalido
+ */
+function getUser() {
+    $user = getPost('user');
+    if (!preg_match('/^[a-zA-Z0-9_]{1,60}$/', $user))
+        $user = ''; //Usuario Invalido
+    return $user;
+}
 
+/**
+ * Trae la clave de Post, lo sanatiza y regresa su hash
+ * @return string Hash de la clave o vacio si es invalido
+ */
+function getPassword() {
+    $hash_cost_log2 = 8;
+    $hash_portable = FALSE;
+    
+    $pass = getPost('pass');
+    if (strlen($pass) > 72)
+        $pass  = ''; //Clave larga
+    if ($pass != ''){
+        $hasher = new PasswordHash($hash_cost_log2, $hash_portable);
+        $hash = $hasher->HashPassword($pass);
+        if (strlen($hash) < 20)
+		$pass  = ''; //Failed to hash new password
+	unset($hasher);
+    }
+    return $pass;
+}
 
-$user = get_post_var('user');
-/* Sanity-check the username, don't rely on our use of prepared statements
- * alone to prevent attacks on the SQL server via malicious usernames. */
-if (!preg_match('/^[a-zA-Z0-9_]{1,60}$/', $user))
-    fail('Invalid username');
-
-$pass = get_post_var('pass');
-/* Don't let them spend more of our CPU time than we were willing to.
- * Besides, bcrypt happens to use the first 72 characters only anyway. */
-if (strlen($pass) > 72)
-    fail('The supplied password is too long');
 ?>
